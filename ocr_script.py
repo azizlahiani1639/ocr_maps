6cca3ecdef045085c90f8631c0efe5927afee5c7
azizlahiani1639 <132688447+azizlahiani1639@users.noreply.github.com>
2024-05-25 17:06:04 +0200

    Update ocr_script.py

--- ocr_script.py
+++ ocr_script.py
@@ -3,16 +3,9 @@ from paddleocr import PaddleOCR
 import cv2
 import os
 
-# Initialize PaddleOCR with English language, angle detection, and CPU usage
 ocr_model = PaddleOCR(lang='en', use_angle_cls=True, use_gpu=False)
-
-# Set the image path
 img_path = 'Genova.png'
-
-# Perform OCR on the image
 result = ocr_model.ocr(img_path)
-
-# Extract detected components
 output_data = []
 for line in result[0]:
     entry = {
@@ -22,7 +15,6 @@ for line in result[0]:
     }
     output_data.append(entry)
 
-# Output result in JSON format
 output_path = 'ocr_output.json'
 with open(output_path, 'w') as f:
     json.dump(output_data, f, indent=4)
